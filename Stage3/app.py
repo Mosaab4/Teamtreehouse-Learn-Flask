9af6769e9d7fee2b7908341eae6499f62fe7b770
Mosaab Ehab <musab.ehab@gmail.com>
2017-05-25 20:40:14 +0200

    add flash messages

--- Stage3/app.py
+++ Stage3/app.py
@@ -1,9 +1,12 @@
 import json
 from flask import (Flask , render_template ,
-        redirect,url_for,request, make_response)
+        redirect,url_for,request, make_response
+        ,flash )
+
 from options import DEFAULTS
 
 app = Flask(__name__)
+app.secret_key = 'sasas2e@E@sc,ozc<ZXC?ZC>||ZXC'
 
 def get_saved_data():
     try:
@@ -27,6 +30,7 @@ def builder():
     )
 @app.route('/save' ,methods=['POST'])
 def save():
+    flash("Alright! That looks awesome!")
     response = make_response(redirect(url_for('builder')))
     data = get_saved_data()
     data.update(dict(request.form.items()))
